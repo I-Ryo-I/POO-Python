eab8a01f197ec557794a049bcf15674deea8b0df
I-Ryo-I <sebastian.moreno1309@gmail.com>
2026-03-12 09:08:25 -0500

    Add files via upload

---
+++ Animales.py
@@ -0,0 +1,64 @@
+class Animales:
+    def sonido(self):
+        print("El animal hace un sonido")
+
+    def Casa(self):
+        print("El animal tiene un hogar")
+
+    def Color(self):
+        print("El animal tiene un color")
+
+
+class Perro(Animales):
+    def sonido(self):
+        print("El perro hace guau guau")
+
+    def Casa(self):
+        print("El perro vive en la casa")
+
+    def Color(self):
+        print("El perro es cafe")
+
+
+class Gato(Animales):
+    def sonido(self):
+        print("El gato hace miau")
+
+    def Casa(self):
+        print("El gato vive en la casa")
+
+    def Color(self):
+        print("El gato es negro")
+
+
+class Vaca(Animales):
+    def sonido(self):
+        print("La vaca hace muu")
+
+    def Casa(self):
+        print("La vaca vive en el establo")
+
+    def Color(self):
+        print("La vaca es blanca con manchas negras")
+
+
+class Leon(Animales):
+    def sonido(self):
+        print("El leon hace roar")
+
+    def Casa(self):
+        print("El leon vive en la selva")
+
+    def Color(self):
+        print("El leon es cafe")
+
+
+class Pollito(Animales):
+    def sonido(self):
+        print("El pollito hace pio")
+
+    def Casa(self):
+        print("El pollito vive en la granja")
+
+    def Color(self):
+        print("El pollito es amarillo")
